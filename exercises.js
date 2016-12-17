a42019684189a01cb37d2febf6eb16dd726d5e13
victorlhlee <victorlhlee@users.noreply.github.com>
2016-12-16 16:55:12 -1000

    Update exercises.js

--- exercises.js
+++ exercises.js
@@ -1,1 +1,96 @@
+/* 1) `Presidents - Accessing values in an Array`
+Declare a variable named `presidents` and set its value to an Array containing the first 5 presidents' last names: `Washington`, `Adams`, `Jefferson`, `Madison`, `Monroe`.
+
+Now write a **for-loop** which iterates through this Array.
+
+Within the **for-loop**, use `console.log` to:
+
+- Log the value of `i`. Use a message like `Value of i is: ` and append the value of `i` to this String
+- Log what is at index `i` in the array. Use a message like `Value at Index is: ` and append the value stored within the Array at that index.*/
+
+/***bonus:** could you take the code inside of this for-loop and encapsulate it inside of a function called `printContent` and still achieve the same result? This new function should accept two parameters.*/
+
+
+
+/* 2) Declare a variable named `stringOfNumbers` and set it's value to `''` (an empty String).
+
+Write a **for-loop** that concatenates a Number value to that string on each iteration, starting at `10` and continuing up to and including `20`. */
+
+
+/***bonus:** could you take the code inside of this for-loop and encapsulate it inside of a function called `appendToString` and still achieve the same result?*/
+
+
+/* 3) `Add only even numbrs to an array`
+Declare a variable named `evenNumberArray` and set its value to an empty array.
+
+Use a for-loop to add only even numbers to an Array. Add `50` even numbers to the `evenNumberArray` starting with the value `0`.*/
+  
+
+/* 4) `Accessing only the odd indexes of an Array - 'Not Even Brah'`
+Someone forgot to fill out this array! Oh noes...
+
+Declare a new variable named `oopsArray` set it's value to be: `[ 'turn' , , 'down' , , 'for' , , 'what' ]`.
+
+Note that every odd index value in `oopsArray` is currently `undefined`. Using a for-loop, add the string `'nope'` to every odd index.
+
+Example result should look like:
+
+`[ 'turn' , 'nope' , 'down' , 'nope' , 'for' , 'nope' , 'what' ]`*/
+
+
+
+/* 5) `Going backwards?!`
+Using a for-loop, iterate through the Array stored at `oopsArray` **backwards**. `console.log` each value in the Array.
+
+example output:
+```
+what
+nope
+for
+nope
+down
+nope
+turn
+```*/
+
+
+
+/* 6) `isNapTime`
+Declare a variable named `isNapTime`. Set it to `false`.
+
+Declare a variable named `napSchedule`. Set it's value to be an `Array` with the values `[false, false, true, false, true, true]`
+
+Declare a function named `nap`. This function takes in a single parameter called `schedule`
+
+- If `schedule` is `true` then use `console.log` to display the message `ZzZzZzZz`
+- otherwise if `schedule` is `false` use `console.log` to display the message `Gotta get to work!` and then change the value of `isNapTime` to `true`
+
+Now, Write a FOR loop that iterates through the `napSchedule` array and runs the function `nap` while passing in the value at the current position of `napSchedule` into the `nap` function.*/
+
+/* Declare a variable named `valuesArray` and set it's value to be an array, `[99, 66, 829, 1941, 8, 76]`.
+
+Declare a function named `copyArray` which takes two arguments: `originArray` and `destinationArray`. Inside of this function you will loop through the contents of `originArray` and push each value into `destinationArray`.
+
+To get started, below your function declaration, call your function and pass in the two variables, `valuesArray` and `copyOfValuesArray`. After that, use `console.log` to to inspect the values of `valuesArray` and `copyOfValuesArray` to make sure they have the same values (which means your function worked!).*/
+
+
+
+/*![final-boss](https://s3.amazonaws.com/uploads.hipchat.com/54891/2015941/zamX8AqbgYw0QJ8/giphy.gif)
+
+ Stage 1 - Only String Values
+Declare a variable named `miscStorage` set it's value to be: `[ [], 'Carrots', 9, 'Beets', {}, {name: "Todd B."}, 'Mush' ]`
+
+Declare a function named `generateArrayOfStrings` which takes a single argument `storage`. This function returns a new Array with only `String` values inside of it.
+
+ Final Form - Change values of objects stored within an Array
+It's that time again, we need to graduate the current class of students and start enrollment for the next class.
+
+Declare a variable named `currentClass` and set it's value to be this [array found here](https://gist.github.com/sgnl/e40879b2249e06ca7811).
+
+Declare a function named `graduateAndSetNewClass`, it takes a single argument called `cohort`.
+
+Your function will iterate through the `cohort` argument and check each student's `enrolled` property.
+
+If the `enrolled` property is set to `true` then change that student's `graduated` property to `true`. Otherwise, if `enrolled` is set to `false` then change `enrolled` to `true` leaving `graduated` alone and unchanged.
+*/
 
