78ebf88ac7d43dce14d5d12f0998dcb92d5ce523
victorlhlee <victorlhlee@users.noreply.github.com>
2016-12-23 11:21:44 -1000

    Update exercises.js

--- exercises.js
+++ exercises.js
@@ -1,4 +1,17 @@
-/* 1) Declare a variable named `presidents` and assign it to an array containing the following Presidents: Washington, Adams, Jefferson, Madison and Monroe.
+/* 1) Ticket Generator
+Write a for-loop that will iterate through 20 numbers (starting at 1 and ending at 20) and console.log the following message:
+"Now serving 1."
+"Now serving 2."
+"Now serving 3."
+.
+.
+.
+"Now serving 20."
+*/
+
+
+/* 2) Dead Presidents
+Declare a variable named `presidents` and assign it to an array containing the following Presidents: Washington, Adams, Jefferson, Madison and Monroe.
  
 Write a for-loop that will iterate through this array and within the for-loop console.log the following:
 "The value at 0 is Washington." 
@@ -7,9 +20,9 @@ Write a for-loop that will iterate through this array and within the for-loop co
 "The value at 3 is "Madison."
 "The value at 4 is Monroe."
 
-What is the length of this array?
+Next, console.log the length of the array.
 
-1-A) Create a function named leaders which will take in a parameter: `person`.
+2-A) Create a function named leaders which will take in a parameter: `person`.
    @param Datatype: String `person`
  
 This function will iterate through the person parameter and console.log the following message for each item in the array:
@@ -18,11 +31,12 @@ This function will iterate through the person parameter and console.log the foll
 */
 
 
-/* 2) Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
+/* 3) Line Number
+Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
 
-Write a for-loop that concatenates a Number value to that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"
+Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"
 
-**bonus:** could you take the code inside of this for-loop and encapsulate it inside of a function called `appendToString` and still achieve the same result?
+3-A) Create a function called `appendToString` and have it produce the same result as above. Console.log your results.
 */
 
 
@@ -72,7 +86,7 @@ Write a function named `copyArray` which takes two arguments: `originArray` and 
    @param Datatype: Array `destinationArray`
 
 Inside of this function write a for-loop that will iterate through the contents of the `originArray` and pushes each element of that array into `destinationArray`. Console.log your result.
-*/
+*/for
 
 
 /* 9) Write a function that will capitalize the first letter in each word in the phrase below. The function will console.log the message: "I've Lived A Life That's Full. I've Traveled Each and Every Highway. But More, Much More Than This. I Did It My Way."  
